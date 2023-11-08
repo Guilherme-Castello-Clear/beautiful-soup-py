7899ae6af40974b38f1247068b383830529694f1
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-08 17:41:13 -0300

    Feat: BS introduction²

--- main.py
+++ main.py
@@ -6,4 +6,19 @@ with open("website.html") as file:
 soup = BeautifulSoup(contents, "html.parser")
 # print(soup.title.string)
 
-print(soup.li)
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# section_heading = soup.find(name="h3", class_="heading")
+# # print(section_heading.getText())
+# company_url = soup.select_one(selector="#name")
+# print(company_url)
+
+print(soup.select(".heading"))
+
